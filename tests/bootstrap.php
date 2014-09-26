ae31a62017cc892a0a5eff08e43dc03a4dcf8716
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2014-09-26 18:11:54 +0100

    Fixing more Travis-related bugs.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,7 +11,7 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../axis.php';
+	require dirname( __FILE__ ) . '/../Axis.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
